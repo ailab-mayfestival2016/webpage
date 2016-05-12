ee7b5798e48fa979a1800cdc40daa76b9ea54921
Petr Khrapchenkov <shigidono@gmail.com>
2016-05-13 01:47:21 +0900

    Improve engine

--- js/engine/block.js
+++ js/engine/block.js
@@ -6,12 +6,10 @@ define(['three', 'engine/utils'], function (_, UTILS) {
         this.block_mesh.scale.y = UTILS.BLOCK_SCALE_FACTOR;
         this.block_mesh.scale.z = UTILS.BLOCK_SCALE_FACTOR;
         this.block_mesh.rotation.x = Math.PI/2;
-        this.circuit_mesh.scale.x = UTILS.BLOCK_SCALE_FACTOR;
-        this.circuit_mesh.scale.y = UTILS.BLOCK_SCALE_FACTOR;
-        this.circuit_mesh.scale.z = UTILS.BLOCK_SCALE_FACTOR;
         this.sh = opts.shift;
         this.freq = opts.frequency;
         this.amp = opts.amplitude;
+        this.meshes = 0;
     }
     block_class.prototype.move = function(x, y) {
         if (this.block_mesh && this.circuit_mesh) {
@@ -55,10 +53,13 @@ define(['three', 'engine/utils'], function (_, UTILS) {
             this.circuit_mesh.position.z = this.block_mesh.position.z;
             this.circuit_mesh.material.uniforms.time.value = elapsedTime;
         } else {
+            if (!this.block_mesh.opacity) {
+                this.block_mesh.opacity = this.block_mesh.material.opacity;
+            }
             this.block_mesh.position.z -= (elapsedTime - this.deadTime) * (elapsedTime - this.deadTime);
             this.circuit_mesh.position.z = this.block_mesh.position.z;
             this.circuit_mesh.material.uniforms.color_on.value.copy(this.circuit_mesh.material.uniforms.color_off.value);
-            this.block_mesh.material.opacity = Math.max(0, 1 - (elapsedTime - Math.min(elapsedTime, this.deadTime + 1.0))/2.0);
+            this.block_mesh.material.opacity = Math.max(0, this.block_mesh.opacity - (elapsedTime - Math.min(elapsedTime, this.deadTime + 1.0))/2.0);
             this.circuit_mesh.material.uniforms.color_off.value.setW(Math.max(0, 1 - (elapsedTime - Math.min(elapsedTime, this.deadTime + 1.0))/2.0));
         }
     }
--- js/engine/scene.js
+++ js/engine/scene.js
@@ -1,6 +1,62 @@
 define(['engine/utils', 'engine/block', 'three', 'OrbitControls', 'StereoEffect', 'DeviceOrientationControls', 'SPE'], function (UTILS, block_class) {
+    function disposeHierarchy(node, callback) {
+        for (var i = node.children.length - 1; i >= 0; i--) {
+            var child = node.children[i];
+            disposeHierarchy(child, callback);
+            callback(child);
+        }
+    }
+
+    function disposeNode(node) {
+        if (node instanceof THREE.Camera) {
+            node = undefined;
+        }
+        else if (node instanceof THREE.Light) {
+            node.dispose();
+            node = undefined;
+        }
+        else if (node instanceof THREE.Mesh) {
+            if (node.geometry) {
+                node.geometry.dispose();
+                node.geometry = undefined;
+            }
+
+            if (node.material) {
+                if (node.material instanceof THREE.MeshFaceMaterial) {
+                    $.each(node.material.materials, function (idx, mtrl) {
+                        if (mtrl.map) mtrl.map.dispose();
+                        if (mtrl.lightMap) mtrl.lightMap.dispose();
+                        if (mtrl.bumpMap) mtrl.bumpMap.dispose();
+                        if (mtrl.normalMap) mtrl.normalMap.dispose();
+                        if (mtrl.specularMap) mtrl.specularMap.dispose();
+                        if (mtrl.envMap) mtrl.envMap.dispose();
+
+                        mtrl.dispose();    // disposes any programs associated with the material
+                        mtrl = undefined;
+                    });
+                }
+                else {
+                    if (node.material.map) node.material.map.dispose();
+                    if (node.material.lightMap) node.material.lightMap.dispose();
+                    if (node.material.bumpMap) node.material.bumpMap.dispose();
+                    if (node.material.normalMap) node.material.normalMap.dispose();
+                    if (node.material.specularMap) node.material.specularMap.dispose();
+                    if (node.material.envMap) node.material.envMap.dispose();
+
+                    node.material.dispose();   // disposes any programs associated with the material
+                    node.material = undefined;
+                }
+            }
+
+            node = undefined;
+        }
+        else if (node instanceof THREE.Object3D) {
+            node = undefined;
+        }
+    }   // disposeNode
     var arcanoid_scene = function(texture_set) {
-        this.blocks = [];
+        this.blocks = {};
+        this.dead_blocks = [];
         this.init(texture_set); 
         window.addEventListener('resize', this.resize.bind(this), false);
         setTimeout(this.resize.bind(this), 1);
@@ -136,14 +192,15 @@ define(['engine/utils', 'engine/block', 'three', 'OrbitControls', 'StereoEffect'
       light.position.set( -1, 0.75, -0.5 );
       this.scene.add( light );
     }
-    arcanoid_scene.prototype.init_blocks = function(map) {
+    arcanoid_scene.prototype.init_materials = function(transparent) {
         var circuit_materials = {};
-        var block_material = new THREE.MeshPhongMaterial({
+        this.block_material = new THREE.MeshPhongMaterial({
             color: 0xe5e5ef,
             shininess: 300.0,
             specular: 0xffffff,
             shading: THREE.FlatShading,
             transparent: true,
+            opacity: transparent?UTILS.DEFAULT_TRANSPARENCY:1.0,
             emissive: 0xf0f0f0,
             normalMap: THREE.ImageUtils.loadTexture(
                 this.texture_set.block_normals
@@ -154,8 +211,8 @@ define(['engine/utils', 'engine/block', 'three', 'OrbitControls', 'StereoEffect'
             envMap: this.cubemap,
         });
 
-        var circuit_texture = THREE.ImageUtils.loadTexture(this.texture_set.pcb);
-        var circuit_gradient_texture = THREE.ImageUtils.loadTexture(this.texture_set.pcb_gradient);
+        this.circuit_texture = THREE.ImageUtils.loadTexture(this.texture_set.pcb);
+        this.circuit_gradient_texture = THREE.ImageUtils.loadTexture(this.texture_set.pcb_gradient);
 
         var vertShader =
             "varying vec2 vUv;\
@@ -188,11 +245,11 @@ define(['engine/utils', 'engine/block', 'three', 'OrbitControls', 'StereoEffect'
             float p = texture2D(pcb, vUv).r;\
             gl_FragColor = vec4(1.0, 1.0, 1.0, p)*mix(color_off, color_on, p*o);\
         }";
-        circuit_texture.wrapS = THREE.RepeatWrapping;
-        circuit_texture.wrapT = THREE.RepeatWrapping;
-        circuit_gradient_texture.wrapS = THREE.RepeatWrapping;
-        circuit_gradient_texture.wrapT = THREE.RepeatWrapping;
-        var mat = new THREE.ShaderMaterial({
+        this.circuit_texture.wrapS = THREE.RepeatWrapping;
+        this.circuit_texture.wrapT = THREE.RepeatWrapping;
+        this.circuit_gradient_texture.wrapS = THREE.RepeatWrapping;
+        this.circuit_gradient_texture.wrapT = THREE.RepeatWrapping;
+        this.circuit_material = new THREE.ShaderMaterial({
             uniforms: {
                 time: {
                     type: 'f',
@@ -216,11 +273,11 @@ define(['engine/utils', 'engine/block', 'three', 'OrbitControls', 'StereoEffect'
                 },
                 pcb: {
                     type: 't',
-                    value: circuit_texture
+                    value: this.circuit_texture
                 },
                 pcb_gradient: {
                     type: 't',
-                    value: circuit_gradient_texture
+                    value: this.circuit_gradient_texture
                 },
             },
             vertexShader: vertShader,
@@ -228,8 +285,7 @@ define(['engine/utils', 'engine/block', 'three', 'OrbitControls', 'StereoEffect'
             transparent: true,
             lights: false
         });
-        var out_blocks = [];
-        for (var i in map) {
+        /*for (var i in map) {
             var block = new block_class(block_material, {
                 shift: Math.random() * 3,
                 frequency: Math.random() * 3 + 2,
@@ -258,15 +314,60 @@ define(['engine/utils', 'engine/block', 'three', 'OrbitControls', 'StereoEffect'
             block.move(map[i].x, map[i].y);
 
             this.blocks.push(block);
+        }*/
+    }
+    arcanoid_scene.prototype.set_blocks = function(map) {
+        for (var i in this.blocks) {
+            this.blocks[i].alive = false;
         }
+        for (var i in map) {
+            var block;
+            if (i in this.blocks) {
+                block = this.blocks[i];
+            } else {
+                var block = new block_class(this.block_material, {
+                    shift: Math.random() * 3,
+                    frequency: Math.random() * 3 + 2,
+                    amplitude: Math.random() * 5 + 2
+                });
+                this.blocks[i] = block;
+                var material = this.circuit_material.clone();
+                material.polygonOffset = true;
+                material.depthWrite = false;
+                material.polygonOffsetFactor = -1;
+                material.polygonOffsetUnits = -2;
+                block.circuit_mesh.material = material;
+                block.rotate(Math.PI / 2, UTILS.randi(0, 4) * Math.PI / 2, 0);
+                block.scale(map[i].x_scale/100.0, map[i].y_scale/100.0, Math.max(map[i].x_scale, map[i].y_scale)/100.0);
+            }
+            //move here circuit material too
+            block.circuit_mesh.material.uniforms.color_on.value = new THREE.Vector4(
+                map[i].color[0],
+                map[i].color[1],
+                map[i].color[2],
+                1.0
+            );
+            block.circuit_mesh.material.uniforms.color_off.value = new THREE.Vector4(
+                block.circuit_mesh.material.uniforms.color_on.value.x / UTILS.PCB_DARKEN_FACTOR,
+                block.circuit_mesh.material.uniforms.color_on.value.y / UTILS.PCB_DARKEN_FACTOR,
+                block.circuit_mesh.material.uniforms.color_on.value.z / UTILS.PCB_DARKEN_FACTOR, 1.0
+            );
+    
+            block.circuit_mesh.material.uniforms.pcb_gradient.value = this.circuit_gradient_texture;
+            block.circuit_mesh.material.uniforms.pcb.value = this.circuit_texture;
+            block.circuit_mesh.material.uniforms.freq.value = block.freq;
+            block.circuit_mesh.material.uniforms.shift.value = block.sh;
+
+            block.move(map[i].x, map[i].y);
+            block.alive = true;
+        }
+        this.update_blocks();
+        this.update_circuits();
         return this.blocks;
     }
 
-    arcanoid_scene.prototype.load_geometry = function() {
-        // TODO: may be unsafe, check what is coming first map or geometry data
-        this.loader.load('resources/models/block.js', function(geometry) {
-            // create a new material
-            this.block_geometry = geometry;
+    arcanoid_scene.prototype.update_blocks = function() {
+        if (this.block_geometry) {
             var material = new THREE.MeshPhongMaterial({
                 color: 0xe5e5ef,
                 shininess: 999.0,
@@ -274,25 +375,30 @@ define(['engine/utils', 'engine/block', 'three', 'OrbitControls', 'StereoEffect'
                 specular: 0xffffff,
                 shading: THREE.FlatShading
             });
-            for (var i = 0; i < this.blocks.length; i++) {
+            for (var i in this.blocks) {
+                if (this.blocks[i].meshes == 2) {
+                    continue;
+                }
+                this.blocks[i].meshes++;
                 var mesh = new THREE.Mesh(
                     this.block_geometry,
                     this.blocks[i].block_mesh.material
                 );
-
                 mesh.position.copy(this.blocks[i].block_mesh.position);
                 mesh.rotation.copy(this.blocks[i].block_mesh.rotation);
                 mesh.scale.copy(this.blocks[i].block_mesh.scale);
                 this.blocks[i].block_mesh = mesh;
                 this.block_holder.add(this.blocks[i].block_mesh);
             }
-        }.bind(this));
-
-        this.loader.load('resources/models/circuit.js', function(geometry) {
-            // create a new material
-            this.circuit_geometry = geometry;
-
-            for (var i = 0; i < this.blocks.length; i++) {
+        }
+    }
+    arcanoid_scene.prototype.update_circuits = function() {
+        if (this.circuit_geometry) {
+            for (var i in this.blocks) {
+                if (this.blocks[i].meshes == 2) {
+                    continue;
+                }
+                this.blocks[i].meshes++;
                 var mesh = new THREE.Mesh(
                     this.circuit_geometry,
                     this.blocks[i].circuit_mesh.material
@@ -302,9 +408,24 @@ define(['engine/utils', 'engine/block', 'three', 'OrbitControls', 'StereoEffect'
                 mesh.rotation.copy(this.blocks[i].circuit_mesh.rotation);
                 mesh.scale.copy(this.blocks[i].circuit_mesh.scale);
                 this.blocks[i].circuit_mesh = mesh;
-                this.scene.add(mesh);
+                this.block_holder.add(mesh);
 
             }
+        }
+
+    }
+    arcanoid_scene.prototype.load_geometry = function() {
+        // TODO: may be unsafe, check what is coming first map or geometry data
+        this.loader.load('resources/models/block.js', function(geometry) {
+            // create a new material
+            this.block_geometry = geometry;
+            this.update_blocks();
+        }.bind(this));
+
+        this.loader.load('resources/models/circuit.js', function(geometry) {
+            // create a new material
+            this.circuit_geometry = geometry;
+            this.update_circuits();
         }.bind(this));
     };
     arcanoid_scene.prototype.create_particle_system = function() {
@@ -393,17 +514,6 @@ define(['engine/utils', 'engine/block', 'three', 'OrbitControls', 'StereoEffect'
         this.explosion_group.mesh.frustumCulled = false;
         this.scene.add(this.group.mesh);
         this.scene.add(this.explosion_group.mesh);
-
-        setInterval(function() {
-            //this.group.triggerPoolEmitter( 1, (this.particle_system_pos.set( randi(30, 100), randi(0, 10), randi(-60, 60) )) )
-        }.bind(this), 1000);
-        var k = 0;
-        setInterval(function(){
-            var n = (k%4)*4 + Math.floor(k/4);
-            this.kill_block(n);
-            k++;
-        }.bind(this), 10000)
-
     }
     arcanoid_scene.prototype.resize = function() {
         var width = this.container.offsetWidth;
@@ -419,6 +529,26 @@ define(['engine/utils', 'engine/block', 'three', 'OrbitControls', 'StereoEffect'
             this.blocks[i].deadTime = this.clock.elapsedTime;
             this.group.triggerPoolEmitter( 1, (this.particle_system_pos.set( this.blocks[i].block_mesh.position.x-50*UTILS.BLOCK_SCALE_FACTOR, this.blocks[i].block_mesh.position.y, this.blocks[i].block_mesh.position.z + 100*UTILS.BLOCK_SCALE_FACTOR )));
             this.explosion_group.triggerPoolEmitter( 1, (this.particle_system_pos.set( this.blocks[i].block_mesh.position.x-50*UTILS.BLOCK_SCALE_FACTOR, this.blocks[i].block_mesh.position.y, this.blocks[i].block_mesh.position.z + 100*UTILS.BLOCK_SCALE_FACTOR )));
+            var block = this.blocks[i];
+            var index = this.dead_blocks.length;
+            this.dead_blocks.push(block);
+            delete this.blocks[i];
+            setTimeout(function() {
+                disposeHierarchy(this.dead_blocks[index].block_mesh, function (child) {
+                    child.parent.remove(child);
+                })
+                disposeHierarchy(this.dead_blocks[index].circuit_mesh, function (child) {
+                    child.parent.remove(child);
+                })
+                this.block_holder.remove(this.dead_blocks[index].block_mesh);
+                this.block_holder.remove(this.dead_blocks[index].circuit_mesh);
+
+                //disposeNode(this.dead_blocks[index].block_mesh);
+                //disposeNode(this.dead_blocks[index].circuit_mesh);
+
+                this.dead_blocks.splice(index, 1);
+                console.log(block);
+            }.bind(this), 10000);
         }
     }
 
@@ -451,8 +581,11 @@ define(['engine/utils', 'engine/block', 'three', 'OrbitControls', 'StereoEffect'
     arcanoid_scene.prototype.animate = function(t) {
         var dt = this.clock.getDelta();
         requestAnimationFrame(this.animate.bind(this));
-        for (var i = 0; i < this.blocks.length; i++) {
+        for (var i in this.blocks) {
             this.blocks[i].animate(this.clock.elapsedTime);
+        }
+        for (var i in this.dead_blocks) {
+            this.dead_blocks[i].animate(this.clock.elapsedTime);
         }
         this.update(dt);
         this.render(dt);
