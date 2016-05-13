e4852f2e8477ec4490fe78ae30ede76b1482cb2e
gokuma <go@kumagai-no-MacBook-Pro.local>
2016-05-13 18:01:59 +0900

    ruleの追加及びdisplay.js/main.js/scene.jsのminification

---
+++ js/displlay_build.js
@@ -0,0 +1,30 @@
+{
+    name: 'display',
+    out: 'build/display.js',
+    paths: {
+        three: 'three.js/build/three',
+        DeviceOrientationControls: 'third-party/threejs/DeviceOrientationControls',
+        OrbitControls: 'third-party/threejs/OrbitControls',
+        StereoEffect: 'third-party/threejs/StereoEffect',
+        SPE: 'third-party/SPE',
+        TrackballControls: 'three.js/examples/js/controls/TrackballControls',
+    },
+    shim: {
+        DeviceOrientationControls: {
+            deps: ['three']
+        },
+        OrbitControls: {
+            deps: ['three']
+        },
+        StereoEffect: {
+            deps: ['three']
+        },
+        SPE: {
+            deps: ['three']
+        },
+        TrackballControls: {
+            deps: ['three']
+        }
+    }
+}
+
